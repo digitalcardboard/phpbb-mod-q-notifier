84a037ddbe542f0889ec8f80e12c6a562d6e9d70
Jason Navarrete <jason@digitalcardboard.com>
2012-12-17 10:52:35 -0800

    renamed file, inital conversion to using parameters

--- phpbb-mod-q-notifier.pl
+++ phpbb-mod-q-notifier.pl
@@ -1,49 +1,70 @@
 #!/usr/bin/perl
  
+use strict;
+use Getopt::Long;
 use DBI;
  
 # forked : https://github.com/digitalcardboard/phpbb-mod-q-notifier
 # origin : http://www.phpbb.com/community/viewtopic.php?f=72&t=832195&start=15
  
 # The next nine lines are the only ones you should need to change. 
-$database_host =        ""; # your database host, the same on phpBB uses
-$database_port =        ""; # your database port, the same one phpBB uses
-$database_name =        ""; # your database name, the same one phpBB uses
-$database_username =    ""; # your database username, the same one phpbBB uses
-$database_password =    ""; # your data base password, the same one phpBB uses
-$table_prefix =         "";
-$forum_address =        ""; # http://www.example.com/forums
-$to =                   ""; # email to address
-$from =                 ""; # email from address
+my $database_host =        ""; # your database host, the same on phpBB uses
+my $database_port =        ""; # your database port, the same one phpBB uses
+my $database_name =        ""; # your database name, the same one phpBB uses
+my $database_username =    ""; # your database username, the same one phpbBB uses
+my $database_password =    ""; # your data base password, the same one phpBB uses
+my $table_prefix =         "";
+my $forum_address =        ""; # http://www.example.com/forums
+my $to =                   ''; # email to address, must be single-quoted
+my $from =                 ''; # email from address, must be single-quoted
  
 # optional changes
-$subject='PHPBB moderation required';
-$counter_file = "check_moderator_count.txt";
-$queue_file = "check_moderator_queue.tmp";
+my $subject='PHPBB moderation required';
+my $counter_file = "check_moderator_count.txt";
+my $queue_file = "check_moderator_queue.tmp";
  
-$post_approval_count = 0;
-$post_report_count = 0;
- 
-open FILE, ">", $queue_file or die $!;
+my $post_approval_count = 0;
+my $post_report_count = 0;
  
 # ---------------------------------------------------------
 # No changes below this line.
 # ---------------------------------------------------------
-$database_name = "dbi:mysql:$database_name:$database_host";
-$dbh = DBI->connect($database_name,$database_username,$database_password);
+
+my %options=();
+my $database_host="localhost";
+my $database_port=3306;
+my $table_prefix='';
+
+(GetOptions( \%options,
+    "d|debug",
+    "e|email",
+    "h|host=s" => \$database_host,
+    "o|port=i" => \$database_port,
+    "u|username=s" => \$database_username,
+    "p|password=s" => \$database_password,
+    "db=s" => \$database_name,
+    "t|table-prefix=s" => \$table_prefix,
+    "u|url=s" => \$forum_address,
+    "to=s" => \$to,
+    "from=s" => \$from
+)) || die "ERROR: Unknown arguments or parameters: %ARGV\n" unless ($ARGV < 0);
+
+open FILE, ">", $queue_file or die $!;
+
+my $database_name = "dbi:mysql:$database_name:$database_host:$database_port";
+my $dbh = DBI->connect($database_name,$database_username,$database_password);
  
 # Check for Posts Needing Approval
-$report_index = 0;
-@col_titles[$report_index] = "Posts";
-# Create a query of the data base.
-$sql = "SELECT `poster_id`, `post_subject`, `post_text` FROM `phpbb_posts` WHERE post_approved=0";
-# Give the data base a chance to prepare. Perhaps it will compile the request.
-$sth = $dbh->prepare($sql);
+# Create a query of the database.
+my $sql = "SELECT `poster_id`, `post_subject`, `post_text` FROM `${table_prefix}phpbb_posts` WHERE post_approved=0";
+# Give the database a chance to prepare. Perhaps it will compile the request.
+my $sth = $dbh->prepare($sql);
 # Run the query.
 $sth->execute || die "Could not execute SQL statement ... maybe invalid?";
+my (@row, @row2, $name, $sth2);
 while (@row=$sth->fetchrow_array) {
    $post_approval_count++;
-   $sql = "SELECT `username` FROM `phpbb_users` WHERE user_id=$row[0]";
+   $sql = "SELECT `username` FROM `${table_prefix}phpbb_users` WHERE user_id=$row[0]";
    $sth2 = $dbh->prepare($sql);
    $sth2->execute || die "Could not execute SQL statement ... maybe invalid?";
    while (@row2=$sth2->fetchrow_array) {
@@ -53,17 +74,15 @@ while (@row=$sth->fetchrow_array) {
 }
  
 # Check for Reported Posts 
-$report_index = 0;
-@col_titles[$report_index] = "Posts";
-# Create a query of the data base.
-$sql = "SELECT `poster_id`, `post_subject`, `post_text` FROM `phpbb_posts` WHERE post_reported=1";
-# Give the data base a chance to prepare. Perhaps it will compile the request.
+# Create a query of the database.
+$sql = "SELECT `poster_id`, `post_subject`, `post_text` FROM `${table_prefix}phpbb_posts` WHERE post_reported=1";
+# Give the database a chance to prepare. Perhaps it will compile the request.
 $sth = $dbh->prepare($sql);
 # Run the query.
 $sth->execute || die "Could not execute SQL statement ... maybe invalid?";
 while (@row=$sth->fetchrow_array) {
    $post_report_count++;
-   $sql = "SELECT `username` FROM `phpbb_users` WHERE user_id=$row[0]";
+   $sql = "SELECT `username` FROM `${table_prefix}phpbb_users` WHERE user_id=$row[0]";
    $sth2 = $dbh->prepare($sql);
    $sth2->execute || die "Could not execute SQL statement ... maybe invalid?";
    while (@row2=$sth2->fetchrow_array) {
@@ -77,6 +96,9 @@ printf (FILE "Posts to Approve: $post_approval_count\n");
 printf (FILE "Posts to Review: $post_report_count\n"); 
 close (FILE);
  
+my $mod_total;
+my $last_mod_total;
+
 $mod_total = $post_approval_count + $post_report_count;
  
 # read counter
@@ -89,7 +111,8 @@ unlink($counter_file);
    
 # send email only if moderation total has changed.
 # this keeps the noise down while allowing regular checks.
-if ($mod_total >0 && $mod_total != $last_mod_total) { 
+if (($mod_total >0 && $mod_total != $last_mod_total)
+    || $options{e}) { 
  
   open FILE, "<", $queue_file or die $!;
    
@@ -115,11 +138,12 @@ if ($mod_total >0 && $mod_total != $last_mod_total) {
   # silence is best here if we are running this from the crontab
   # as output gets forwarded to the machine admin.
   # run script with '-d' parameter to force output
-    if ( $ARGV[0] eq "-d" ) {
+    if ( $options{d} ) {
       print "A message has been sent from $from to $to\n";
     }
 } else {
-    if ( $ARGV[0] eq "-d" ) {
+    if ( $options{d} ) {
         print "Moderation queue is empty\n";
     }
 }
+
